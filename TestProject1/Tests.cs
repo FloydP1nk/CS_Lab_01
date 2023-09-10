d7fd050928712421b04506b7ca6625704afcd53a
FloydP1nk <113801739+FloydP1nk@users.noreply.github.com>
2023-09-10 14:28:42 +0300

    commit 3

--- TestProject1/Tests.cs
+++ TestProject1/Tests.cs
@@ -13,8 +13,18 @@ namespace TestProject1
             int side1 = 2;
             int side2 = 4;
             Rectangle rect = new Rectangle(side1, side2);
-            Assert.AreEqual(8,rect.Area);
-            Assert.AreEqual(12,rect.Perimeter);
+            Assert.AreEqual(8, rect.Area);
+            Assert.AreEqual(12, rect.Perimeter);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Point A = new Point(0, 0);
+            Point B = new Point(0, 4);
+            Point C = new Point(3, 0);
+            Figure figure = new Figure(A, B, C);
+            Assert.AreEqual(4, figure.LengthSide(A, B));
         }
     }
 }
